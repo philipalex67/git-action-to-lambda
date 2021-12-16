d46be4c6971e36fc308d79b733494e8698024f7f
Philip Alexander <a388504@AMRL20FMC0GXUG4.amer.pfizer.com>
2021-12-16 20:14:18 +0530

    test commit 2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
    const response = {
        statusCode: 200
-       body: JSON.stringify('Updated Through Github Action stage to perform some uhanget  by philip alexander'),
+       body: JSON.stringify('Updated Through Github Action stage to  some uhanget  by philip alexander'),
    };
    return response;
 };
