f533d1920c43b0f37ca20e600b278f0173d46e54
Philip Alexander <a388504@AMRL20ME90STM40.amer.pfizer.com>
2021-12-20 11:53:17 +0530

    Test commit 3

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 exports.handler = async (event) => {
    const response = {
        statusCode: 200,
+<<<<<<< HEAD
        body: JSON.stringify('Updated Through Github Action Demo after changing PR test by philip alexander'),
+=======
+       body: JSON.stringify('Updated Through Github Action Dev Demo after changing branch protection  by philip alexander'),
+>>>>>>> 2596953 (Test commit 3)
    };
    return response;
 };
